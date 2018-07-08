4d385fc087e83a9048efd4b22ab4332a878a6987
Ian Wells <ianwellsh@hotmail.com>
2018-07-08 18:46:18 -0400

    commit[2]
    listas para filas y columnas para las pistas.

--- main.py
+++ main.py
@@ -1,32 +1,39 @@
 import numpy as np
-
-path = '/users/personal/documents/nonogram.txt'
-
-file = open(path,'r')
-
-def readFile(file):#reads the input from file nonogram.txt
-    n = archivo.readline()
-    m = archivo.readline()
-    n = int(n)
-    m = int(m)
     
-    createNonogram(n,m)
-    
-    rowHints = np.array(n,10)
-    columnHints = np.array(m,10)
-    
-    
-    for i in range(n):
-        
-    
-    for i in range(m):
-        
-    
-
 def createNonogram(n,m):#n and m refers to lenght of matrix
     nonogram=np.zeros(n,m)
     return nonogram
+
 def imprimirNonograma(state):
 
 def leerPistas():
     listaIzquierda     
+
+path = '/users/personal/documents/nonogram.txt'
+
+file = open(path,'r')
+    
+n = archivo.readline()
+m = archivo.readline()
+n = int(n)
+m = int(m)
+    
+createNonogram(n,m)
+    
+rowHints = []
+columnHints = []
+
+
+for i in range(n):
+    hints = archivo.readline() #se lee una linea del archivo con pistas 
+    elementList = [] 
+    for element in hints.split():
+        elemntList.add(int(elemnt)) #se transforman los elementos en numero
+        rowHints.add(elemntList)
+    
+for i in range(m):
+    hints = archivo.readline() #se lee una linea del archivo con pistas 
+    elementList = [] 
+    for element in hints.split():
+        elemntList.add(int(elemnt)) #se transforman los elementos en numero
+        columnHints.add(elemntList)
