662081b874ad0bc32505fa58cb9b6655cba01246
Ian Wells <ianwellsh@hotmail.com>
2018-07-08 17:49:45 -0400

    readfile

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import numpy as np
 
+path = '/users/personal/documents/nonogram.txt'
 
-def createNonogram(n,m):#n and m refers to lenght of matrice
+file = open(path,'r')
+
+def readFile(file):#reads the input from file nonogram.txt
+    n = archivo.readline()
+    m = archivo.readline()
+    n = int(n)
+    m = int(m)
+    
+    createNonogram(n,m)
+    
+    rowHints = np.array(n,10)
+    columnHints = np.array(m,10)
+    
+    
+    for i in range(n):
+        
+    
+    for i in range(m):
+        
+    
+
+def createNonogram(n,m):#n and m refers to lenght of matrix
     nonogram=np.zeros(n,m)
     return nonogram
 def imprimirNonograma(state):
